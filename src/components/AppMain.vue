8e3db39c03f398aea83cc51e16bc57d50bc5001a
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-15 15:24:51 +0100

    add-main-structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,25 @@ export default {
 </script>
 
 <template>
-  <main>MAIN</main>
+  <main>
+    <div class="container">
+      <h1>CONTENTS HERE...</h1>
+    </div>
+  </main>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixin" as *;
+
+main {
+  background-color: $primary-color;
+  height: 120px; //rimuovere
+  .container {
+    @include content();
+    h1 {
+      color: white; //rimuovere
+    }
+  }
+}
+</style>
