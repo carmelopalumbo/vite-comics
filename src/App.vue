040b6c6e994599f56d64f694174ec9e149aaf4ff
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-15 14:08:54 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="container">
+    <h1>App</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
