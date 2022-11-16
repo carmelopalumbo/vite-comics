92f1d2f8dd865d68b1efb28784bbe44ff27071d8
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-16 09:58:24 +0100

    fix-selectors-h4-footer

--- src/components/AppFooterBottom.vue
+++ src/components/AppFooterBottom.vue
@@ -10,7 +10,7 @@ export default {
     <div class="content">
       <h4>SIGN-UP NOW!</h4>
       <div class="follow-us">
-        <h3>FOLLOW US</h3>
+        <h4>FOLLOW US</h4>
         <img src="../assets/img/footer-facebook.png" alt="facebook" />
         <img src="../assets/img/footer-twitter.png" alt="twitter" />
         <img src="../assets/img/footer-youtube.png" alt="youtube" />
@@ -33,23 +33,24 @@ footer {
     @include justify-between();
     height: 100%;
     align-items: center;
-    h4 {
+    & > h4 {
       margin-left: 40px;
       padding: 13px;
       border: 2px solid $secondary-color;
       color: white;
       cursor: pointer;
     }
-    h3 {
-      color: $secondary-color;
-      margin-right: 30px;
-      padding-top: 5px;
-    }
     .follow-us {
       display: flex;
       img {
         margin: 0 7px;
         cursor: pointer;
+      }
+      & > h4 {
+        color: $secondary-color;
+        margin-right: 30px;
+        padding-top: 5px;
+        font-size: 1.1rem;
       }
     }
   }
