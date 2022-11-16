804f6d8da2389075a59e92af5bcd5922629594ec
carmelopalumbo <carmelopalumbo@outlook.it>
2022-11-16 16:11:48 +0100

    add-price-on-hover

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -11,11 +11,17 @@ export default {
   <div class="card">
     <img :src="card.thumb" :alt="card.series" />
     <h4>{{ card.series }}</h4>
+    <p>
+      {{ card.price }} <br />
+      BUY NOW!
+    </p>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
 .card {
+  position: relative;
   width: calc(100% / 6);
   padding: 20px;
   cursor: pointer;
@@ -24,6 +30,7 @@ export default {
     height: 150px;
     object-fit: cover;
     object-position: top;
+    transition: 0.4s all;
   }
   h4 {
     color: white;
@@ -32,6 +39,24 @@ export default {
     vertical-align: middle;
     display: inline-block;
     text-transform: uppercase;
+  }
+  p {
+    text-align: center;
+    position: absolute;
+    top: 30%;
+    left: 25%;
+    opacity: 0;
+    transition: 0.4s all;
+    color: white;
+    -webkit-text-stroke: 0.5px $secondary-color;
+    font-weight: bold;
+    font-size: 0.9rem;
+  }
+  &:hover p {
+    opacity: 1;
+  }
+  &:hover img {
+    filter: brightness(0.3);
   }
 }
 </style>
